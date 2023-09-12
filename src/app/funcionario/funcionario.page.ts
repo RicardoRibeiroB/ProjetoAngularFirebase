205d1b62b3cf1d5335e2cf04b9f0055bd2d7ace2
RicradoRibeiroB <ricardoribeiro062106@gmail.com>
2023-09-12 18:32:02 -0300

    commit

--- src/app/funcionario/funcionario.page.ts
+++ src/app/funcionario/funcionario.page.ts
@@ -9,6 +9,11 @@ export class FuncionarioPage {
 
   isLoading: boolean = false;
   funcionarios: any;
+  insirir = 1;
+
+  constructor(){
+    this.getFuncionarios()
+  }
 
   getFuncionarios(){
     this.isLoading = true;
@@ -26,26 +31,48 @@ export class FuncionarioPage {
       console.log('processo finalizado');
     })
   }
-  getFuncionariosRemove(){
+  removerFuncionario(CodFun: any){
     this.isLoading = true;
-    fetch('http://localhost/api/funcionarios/remover_funcionario.php',
-			{
-			  method: 'POST',
-			  headers: {
-			    'Content-Type': 'application/json',
-			  },
-			  body: JSON.stringify({ CodFun: '123', Acao: 'remover'})
-			}
-		)
-    .then(response => response.json())
-    .then(response => {
-      console.log(response);
+    
+    fetch('http://localhost/exercicio/funcionario/remover_funcionario.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ CodFun: CodFun}),
+    })
+    .then(resp => resp.json())
+    .then(dados => {
+      console.log(dados);
+      this.getFuncionarios();
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      this.isLoading = false;
+      console.log('processo finalizado');
+    })
+  }
+  
+  mostrarinput(valor: any){
+    this.insirir = valor;
+  }
+
+  adicionarFuncionarios(){
+    this.isLoading = true;
+    fetch('http://localhost/exercicio/funcionario/insirir_funcionario.php')
+    .then(insirir => insirir.json())
+    .then(insirir => {
+      console.log(insirir);
+      this.funcionarios = insirir['insirir'];
     })
     .catch(erro => {
       console.log(erro);
     })
     .finally(()=>{
       this.isLoading = false;
+      console.log('processo finalizado');
     })
   }
 }
