2db5b99835dbd8fe7fc40a6b5f206b4ccf5cf8be
RicardoRibeiroB <ricardoribeiro062106@gmail.com>
2023-09-21 18:30:44 -0300

    commit all

--- src/app/funcionario/funcionario.page.ts
+++ src/app/funcionario/funcionario.page.ts
@@ -10,14 +10,40 @@ export class FuncionarioPage {
   isLoading: boolean = false;
   funcionarios: any;
   insirir = 1;
-
+  mensagem: any;
   constructor(){
     this.getFuncionarios()
+    this.getAllFuncionarios()
   }
 
   getFuncionarios(){
-    this.isLoading = true;
-    fetch('http://localhost/exercicio/funcionario/listar_funcionario.php')
+    fetch('http://localhost/exercicio/funcionario/listar_funcionario.php'
+		)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      this.funcionarios = response['funcionarios'];
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{
+      this.isLoading = false;
+      console.log('processo finalizado');
+    })
+  }
+  getAllFuncionarios(){
+    let funcionario = { CodFun: '' };
+
+    fetch('http://localhost/exercicio/funcionario/listarTodos_funcionario.php',
+			{
+			  method: 'POST',
+			  headers: {
+			    'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify(funcionario)
+			}
+		)
     .then(response => response.json())
     .then(response => {
       console.log(response);
@@ -33,18 +59,22 @@ export class FuncionarioPage {
   }
   removerFuncionario(CodFun: any){
     this.isLoading = true;
-    
-    fetch('http://localhost/exercicio/funcionario/remover_funcionario.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ CodFun: CodFun}),
-    })
+		let funcionario = { CodFun: CodFun };
+
+    fetch('http://localhost/exercicio/funcionario/remover_funcionario.php',
+			{
+			  method: 'DELETE',
+			  headers: {
+			    'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify(funcionario)
+			}
+		)
     .then(resp => resp.json())
     .then(dados => {
       console.log(dados);
-      this.getFuncionarios();
+      this.funcionarios = dados['mensagem'];
+      this.getAllFuncionarios();
     })
     .catch(error => {
       console.log(error);
@@ -59,13 +89,22 @@ export class FuncionarioPage {
     this.insirir = valor;
   }
 
-  adicionarFuncionarios(){
+  adicionarFuncionarios(form: any){
+    let funcionarios = form;
+    // console.log(form);
     this.isLoading = true;
-    fetch('http://localhost/exercicio/funcionario/insirir_funcionario.php')
-    .then(insirir => insirir.json())
-    .then(insirir => {
-      console.log(insirir);
-      this.funcionarios = insirir['insirir'];
+    fetch('http://localhost/exercicio/funcionario/insirir_funcionario.php',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(funcionarios)
+    })
+    .then(resp => resp.json())
+    .then(resp=> {
+      console.log(resp);
+      this.mensagem = resp.mensagem;
     })
     .catch(erro => {
       console.log(erro);
